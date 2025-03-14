fd5b842c600cdc1b1f4a461494e96cba65895ef8
jaredcourage <145121823+jaredcourage@users.noreply.github.com>
2025-03-14 10:57:20 +0000

    Create Air travel.php

---
+++ Air travel.php
@@ -0,0 +1,531 @@
+<?php
+include('header.php'); 
+?>
+<!-- End  -->
+
+
+
+       
+    <section class="Immigration-section">
+
+     
+
+    <div class="ancho-link"><a href="index.php">canada.ca</a>
+        <i class="fa-solid fa-chevron-right" style="color: purple;"></i><a href="" style="color:   purple;" href="travel home.php">Travel Home</a>
+    </div>
+
+  
+<main class="emlpo">
+
+    <div>
+   <h1>Air travel</h1>
+     <div class="red-rule-3"></div>
+     <p>Preparing for a flight, airport security, health concerns and 
+        more.
+       </p>
+  
+    </div>
+
+    
+
+  
+ 
+
+     <div class="immigration-div">
+    
+   <div class="image-application"> <img src="https://travel.gc.ca/vt/images/topic/air-travel-airport-waiting.jpg">
+    <div style="display: none;" class="text-bot ">
+    <a style="color: white; padding-left: 17px; display: none; "></a> 
+    <p>
+    </p>
+</div>
+
+ </div> 
+
+
+</main>
+        
+ <!-- the new box div below-->
+
+
+
+
+
+
+    </section>
+
+
+
+
+
+  
+
+<div class="clearfix"></div>
+
+
+<!--- for the request section -->
+
+
+
+
+
+
+
+
+
+<div class="clearfix"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--- the jobs sector -->
+
+<section class="another-sector">
+    <p class="work-h1">Before you go
+    </p>
+    <div class="another-div">
+       <div class="formular">
+<div class="a-b">
+   <a href="#"> What you can bring on a plane</a>
+<p>Items that you can or cannot bring on a plane</p>
+</div>
+<div class="a-b">
+   <a href="#">Carry-on baggage
+   </a>
+<p>Carry-on baggage definitions, regulations and policies</p>
+
+<div class="a-b">
+   <a href="#">Liquids, aerosols and gels</a>
+   <p>
+    How to properly pack your liquids, non-solid food, personal and toiletry items</p>
+   </div>
+
+       </div>
+
+       <div class="formular">
+
+   <div class="a-b">
+       <a href=""> 
+        Pre-boarding checklist
+       </a>
+       <p>A checklist of what you need to remember before you board the plane </p>
+       </div>
+
+       <div class="a-b">
+       <a href="#">Visit your children or grandchildren</a>
+       <p>
+
+        Who is eligible and how to apply for a super visa that lets you stay with family in Canada for 5 years at a time
+       </p>
+           </div>
+
+           
+
+               <div class="formular">
+
+           
+
+               
+
+                  
+                       </div>
+                       </div>
+      
+   
+  
+    </div>
+     </section>
+
+
+
+
+
+
+     <!--- the jobs sector -->
+
+<section class="another-sector">
+    <span class="work-h1">Safety and security</span>
+    <div class="another-div">
+       <div class="formular">
+<div class="a-b">
+   <a href="#"> Airport security screening</a>
+<p>Information to guide you through the airport security screening process.</p>
+</div>
+<div class="a-b">
+   <a href="#">Airport security screening wait times</a>
+<p>Security screening wait times at airports in Canada</p>
+</div>
+
+<div class="a-b">
+   <a href="#">In-flight safety</a>
+   <p>
+    Aircraft safety procedures and advice</p>
+   </div>
+
+       </div>
+
+       <div class="formular">
+
+   
+
+      
+
+                   
+      
+   
+  
+    </div>
+     </section>
+
+
+
+<!------>
+
+     <section class="another-sector">
+        <span class="work-h1">More information</span>
+        <div class="another-div">
+           <div class="formular">
+    <div class="a-b">
+       <a href="#"> Duty-free purchases as carry-on baggage</a>
+    <p>Ensure that duty-free liquids, aerosols and gels are sealed in official security bags</p>
+    </div>
+    <div class="a-b">
+       <a href="#">CANPASS - Private aircraft</a>
+    <p>CANPASS cards allow private aircraft to clear customs and immigration quickly and securely</p>
+    </div>
+    
+    <div class="a-b">
+       <a href="#">Air passenger rights</a>
+       <p>
+        Airlines have obligations to their passengers</p>
+       </div>
+    
+           </div>
+    
+           <div class="formular">
+    
+       
+    
+          
+    
+                       
+          
+       
+      
+        </div>
+         </section>
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+     
+
+
+
+
+
+
+
+
+
+     <!---- the features list for phone  -->
+
+<main class="main-feature">
+   
+    <section class="features-section">
+     <h2 style="margin-left: 10px;">Features</h2>
+ 
+ 
+ 
+    
+     
+    
+ 
+    
+ 
+ 
+ 
+ 
+         
+ 
+
+   
+
+
+
+
+
+
+    <div class="features-div">
+        <img src="https://travel.gc.ca/vt/images/promo/dual-citizen.jpg">
+        <a style="color:blueviolet ;">Dual Canadian citizen : fly to Canada a valid Canadaian passport </a>
+        <p>You need a valid Canadian passport to board a flight to Canada. 
+        </p>
+      
+     
+    <!-- the phone div below-->
+    
+       </div>
+
+
+
+       <div class="features-div">
+        <img src="https://travel.gc.ca/vt/images/promo/customs-plane.jpg">
+        <a style="color:blueviolet ;">Travelling outside Canada </a>
+        <p>Find helpul information on heathy and safety 
+            Travel documents, baggage, airport security,
+            Country advice and more
+        </p>
+      
+     
+    <!-- the phone div below-->
+    
+       </div>
+
+
+
+
+
+       <div class="features-div">
+        <img src="https://travel.gc.ca/vt/images/promo/travel-health-kit-red.jpg">
+        <a style="color:blueviolet ;">Travel insurance </a>
+        <p>Important information on travel insurance 
+            and why you shouldn't leave the country 
+            without
+        </p>
+      
+     
+    <!-- the phone div below-->
+    
+       </div>
+
+
+ 
+       </section>
+     </main>
+ 
+ 
+
+
+
+<!---- the features list 2 for laptop  -->
+
+<main class="feature-22">
+   
+    <section class="features-section-2">
+     <h2>Features</h2>
+ 
+ 
+ 
+ 
+     <div class="lspp">
+    
+ 
+     <div class="laptop-div">
+        <img src="https://travel.gc.ca/vt/images/promo/dual-citizen.jpg">
+     <div class="l">
+        <a style="color:blueviolet ;">Dual Canadian citizen : fly to Canada a valid Canadaian passport </a>
+        <p>You need a valid Canadian passport to board a flight to Canada. 
+        </p>
+ </div>
+ <!-- the phone div below-->
+ </div>
+
+
+
+ <div class="lspp">
+    
+ 
+    <div class="laptop-div">
+ <img src="https://travel.gc.ca/vt/images/promo/customs-plane.jpg">
+    <div class="l">
+        <a style="color:blueviolet ;">Travelling outside Canada </a>
+        <p>Find helpul information on heathy and safety 
+            Travel documents, baggage, airport security,
+            Country advice and more
+        </p>
+</div>
+<!-- the phone div below-->
+</div>
+
+
+
+<div class="lspp">
+    
+ 
+    <div class="laptop-div">
+        <img src="https://travel.gc.ca/vt/images/promo/travel-health-kit-red.jpg">
+    <div class="l">
+        <a style="color:blueviolet ;">Travel insurance </a>
+        <p>Important information on travel insurance 
+            and why you shouldn't leave the country 
+            without
+        </p>
+</div>
+<!-- the phone div below-->
+</div>
+ 
+    
+ 
+ 
+ 
+ 
+         
+ 
+ 
+    
+ 
+ 
+ 
+
+         
+        
+         </div>
+ 
+ </div>
+ <!----the phone -->
+ 
+       </section>
+ 
+     </main>
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- the translator -->
+
+
+
+  
+    
+       
+
+
+    
+       
+
+          <!-- ending of the content tag for translation  -->
+         
+
+
+
+
+
+
+
+
+
+
+        
+ 
+
+
+
+
+
+</div>
+<!-- content for translation-->
+
+
+
+
+
+
+
+<?php
+include('footer.php'); 
+?>
+</body>
+</html>
+<script src="search.js"></script>
