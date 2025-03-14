e2db91686a62b106cc30d00c9266ce28bf6eb89a
jaredcourage <145121823+jaredcourage@users.noreply.github.com>
2025-03-14 10:58:14 +0000

    Create Benefits home:.php

---
+++ Benefits home:.php
@@ -0,0 +1,390 @@
+<?php
+include('header.php'); 
+?>
+<!-- End  -->
+
+
+
+       
+    <section class="Immigration-section">
+
+     
+
+    <div class="ancho-link"><a href="">canada.ca</a>
+        <i class="fa-solid fa-chevron-right" style="color: purple;"></i><a style="color:   purple;" href="Benefits home:.php">Business and industry</a>
+    </div>
+
+  
+<main class="emlpo">
+
+    <div>
+   <h1>Benefits</h1>
+     <div class="red-rule-3"></div>
+     <p>
+        Includes Employment Insurance, dental coverage, child benefit, 
+        pensions and benefits for housing, student aid,
+         family, disabilities and after a death.
+       </p>
+  
+    </div>
+
+    
+
+  
+ 
+
+     <div class="immigration-div">
+    
+   <div class="image-application"> <img src="https://www.canada.ca/content/dam/esdc-edsc/images/services/benefits/theme/benefits-theme.jpg">
+    <div style="display: none;" class="text-bot ">
+    <a style="color: white; padding-left: 17px; display: none; "></a> 
+    <p>
+    </p>
+</div>
+
+ </div> 
+
+
+</main>
+        
+ <!-- the new box div below-->
+
+
+
+
+
+
+    </section>
+
+
+
+
+
+  
+
+<div class="clearfix"></div>
+
+
+<!--- for the request section -->
+
+
+
+
+
+
+
+
+
+<div class="clearfix"></div>
+
+
+
+
+
+
+
+
+
+<!-- ending of the content of the job section  -->
+           
+<section class="second-request">
+       
+    <div class="request-div">
+
+    <h1>Most Requested</h1>     
+    <ul class="request">
+    <div class="formular-2">
+    <li ><a >Apply for Employment Insurance</a></li>
+    <li ><a >Canada Buys</a></li>
+    <li><a>Procurement support for business</a></li>
+    <li><a >Sign in to a Government of Canada online account</a></li>
+   </div>
+   
+    <div class="formular-3">
+    <li><a>Apply for Canada student loans</a></li>
+    <li><a>Canada apprentice loan</a></li>
+    <li><a>Get a work permit</a></li>
+    <li><a>Do your 2024 taxes</a></li>
+    </div>
+
+   
+        
+      
+       
+    
+    <div class="">
+    
+    </div>
+
+</div>
+    </ul>
+ 
+      </section>
+      <div class="clearfix"></div>
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+<!--- the jobs sector -->
+
+<section class="another-sector">
+    <p class="work-h1">
+    </p>
+    <div class="another-div">
+       <div class="formular">
+<div class="a-b">
+   <a href="#"> Employment Insurance benefits and leave</a>
+<p>Temporary benefits for workers, sickness, fishing and family-related 
+    benefits. How to apply online and submit a report.</p>
+</div>
+<div class="a-b">
+   <a href="#">Benefits finder
+   </a>
+<p>A tool to help you find federal, provincial and territorial benefits and services you may be eligible to receive.</p>
+
+<div class="a-b">
+   <a href="#">Benefits payment dates</a>
+   <p>
+    Payment dates for recurring Government of Canada benefit payments.
+</p>
+   </div>
+
+
+   <div class="a-b">
+    <a href="#">Benefits by audience</a>
+    <p>
+        Benefits and programs, including eligibility.
+ </p>
+    </div>
+
+
+    <div class="a-b">
+        <a href="#">Student aid and education planning</a>
+        <p>
+            Plan for an education, save, budget. Explore student aid and career options.
+     </p>
+        </div>
+
+
+
+
+
+
+       </div>
+
+       <div class="formular">
+
+   
+
+               <div class="formular">
+
+           
+
+               
+
+                  
+                       </div>
+                       </div>
+      
+   
+  
+    </div>
+     </section>
+
+
+
+
+      <!---- the features list for phone  -->
+
+ <main class="main-feature">
+   
+    <section class="features-section">
+     <h2 style="margin-left: 10px;">Features</h2>
+ 
+ 
+ 
+    
+     
+     <div class="features-div">
+      
+       
+      
+        <video width="400" controls>
+            <source src="https://www.canada.ca/content/dam/esdc-edsc/videos/services/benefits/publicpensions/public-pensions-video/AT1679_Public_Pensions_EN_4_OC.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+        <a style="color:blueviolet; ">Video: Public Pensions </a> 
+        <p>Find out what type of information is 
+        available on public pension and where 
+        to find in on our website
+        </p>
+  
+ <!-- the phone div below-->
+ </div>
+ 
+
+       </section>
+     </main>
+ 
+
+
+
+<!---- the features list 2 for laptop  -->
+
+<main class="feature-22">
+   
+    <section class="features-section-2">
+     <h2>Features</h2>
+ 
+ 
+ 
+ 
+     <div class="lspp">
+    
+ 
+     <div class="laptop-div">
+
+        <video width="400" controls>
+            <source src="https://www.canada.ca/content/dam/esdc-edsc/videos/services/benefits/publicpensions/public-pensions-video/AT1679_Public_Pensions_EN_4_OC.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+     
+    <div class="l">
+        <a style="color:blueviolet; ">Video: Public Pensions </a> 
+        <p>Find out what type of information is 
+        available on public pension and where 
+        to find in on our website
+        </p>
+ </div>
+ <!-- the phone div below-->
+ </div>
+
+
+
+    
+ 
+
+
+    
+ 
+    
+ 
+ 
+         
+ 
+ 
+    
+ 
+ 
+ 
+
+         
+        
+         </div>
+ 
+ </div>
+ <!----the phone -->
+ 
+       </section>
+ 
+     </main>
+
+
+
+
+
+
+
+
+
+    <section class="funding-grants">
+        <div class="grants">
+            <h2>From:</h2>
+            <ol class="custom-list">
+                
+                <li><a style="color: #26374a" href="#">Innovation, Science and Economic </a></li>
+                <li><a style="color: #26374a" href="#">Public Services and Procurement Canada</a></li>
+                <li><a style="color: #26374a" href="#">Canadian Heritage</a></li>
+            
+            </ol>
+        </div>
+    </section>
+
+
+
+
+   
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    <!-- the translator -->
+
+
+
+  
+    
+       
+
+
+    
+       
+
+          <!-- ending of the content tag for translation  -->
+         
+
+
+
+
+
+
+
+
+
+
+        
+ 
+
+
+
+
+
+</div>
+<!-- content for translation-->
+
+
+
+<?php
+include('footer.php'); 
+?>
+</body>
+</html>
+<script src="search.js"></script>
