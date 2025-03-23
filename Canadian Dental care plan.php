66b4ac559312c1b958a7369331ad7af5afcdc616
jaredcourage <145121823+jaredcourage@users.noreply.github.com>
2025-03-23 07:20:54 +0100

    Create Canadian Dental care plan.php

---
+++ Canadian Dental care plan.php
@@ -0,0 +1,299 @@
+<?php
+include('header.php'); 
+?>
+<!-- End  -->
+
+
+
+       
+    <section class="Immigration-section">
+
+     
+
+    <div class="ancho-link"><a href="">canada.ca</a>
+        <i class="fa-solid fa-chevron-right" style="color: purple;"></i><a style="color:   purple;" href="Canadian Dental Care Plan.php">Canadian Dental Care Plan</a>
+    </div>
+
+  
+<main class="emlpo">
+
+    <div>
+   <h1>Canadian Dental Care Plan</h1>
+     <div class="red-rule-3"></div>
+     <p>
+        The Canadian Dental Care Plan (CDCP) is helping make the cost
+         of dental care more affordable for eligible Canadian residents.
+       </p>
+  
+    </div>
+
+    
+
+  
+ 
+
+     <div style="display: none;" class="immigration-div">
+    
+   <div class="image-application"> <img src="https://www.canada.ca/content/dam/esdc-edsc/images/services/benefits/theme/benefits-theme.jpg">
+    <div style="display: none;" class="text-bot ">
+    <a style="color: white; padding-left: 17px; display: none; "></a> 
+    <p>
+    </p>
+</div>
+
+ </div> 
+
+
+</main>
+        
+ <!-- the new box div below-->
+
+
+
+
+
+
+    </section>
+
+
+
+
+
+  
+
+<div class="clearfix"></div>
+
+
+<!--- for the request section -->
+
+
+
+
+<!--- the applicants date-->
+
+<div class="checkers">
+<h3>Applications now open for:</h3>
+<ul>
+    <li><i class="fas fa-check check-icon"></i> Seniors aged 65 and over</li>
+    <li><i class="fas fa-check check-icon"></i> Children under the age of 18</li>
+    <li><i class="fas fa-check check-icon"></i> Adults with a valid federal Disability Tax Credit certificate for 2023</li>
+</ul>
+</div>
+
+<!-- the end-->
+
+
+
+
+
+
+<section class="chin-section">
+
+<h1>Section</h1>
+    <div class="section-devide">
+
+          <div class="jared">  
+        <div class="timeline">
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+        </div>
+
+    </div>
+
+    <div class="jared">  
+        <div class="section-text">
+         <li><a>Do you qualify</a></li>  
+          <p>Find out if you're eligible for the CDCP.</p>
+        </div>
+
+
+        <div class="section-text">
+         <li><a class="second-aaa">When can you visit an oral health provider</a> </li> 
+          <p>Find out about your CDCP benefit start date,<br>
+             and find an oral health care provider.</p>
+        </div>
+
+
+        <div class="section-text">
+           <li><a>What services are covered</a> </li> 
+            <p>Find out which oral health care services are covered<br> 
+                and how much the CDCP will pay toward those services.</p>
+          </div>
+      </div>
+
+
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+<div class="clearfix"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+    <section class="funding-grants">
+        <div class="grants">
+            <h1>For oral health providers and stakeholders</h1>
+            <h2 style="color: rgb(56, 55, 55); font-size: 20px;
+            word-spacing: 4px;">Information is available for oral health professionals,
+                 including information on how to participate, CDCP
+                  fees and coordination of benefits.</h2>
+            <ol class="custom-list">
+                
+                <li><a style="color: #26374a" href="#">Information for oral health professionals </a></li>
+                <li><a style="color: #26374a" href="#">CDCP promotional toolkit</a></li>
+                
+            
+            </ol>
+        </div>
+    </section>
+
+
+
+    <section class="funding-grants">
+        <div class="grants">
+            <h1>Contact us</h1>
+            <h2 style="color: rgb(56, 55, 55); font-size: 20px;
+            word-spacing: 4px;">If you have questions about the CDCP, please contact us.</h2>
+            <ol class="custom-list">
+                
+            </ol>
+        </div>
+    </section>
+
+
+
+
+
+
+    <section class="funding-grants">
+        <div class="grants">
+            <h1>Related link</h1>
+            <h2 style="color: rgb(56, 55, 55); font-size: 20px;
+            word-spacing: 4px;">Information is available for oral health professionals,
+                 including information on how to participate, CDCP
+                  fees and coordination of benefits.</h2>
+            <ol class="custom-list">
+                
+                <li><a style="color: #26374a" href="#">Canadian Dental Care Plan statistics</a></li>
+               
+                
+            
+            </ol>
+        </div>
+    </section>
+
+
+
+   
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    <!-- the translator -->
+
+
+
+  
+    
+       
+
+
+    
+       
+
+          <!-- ending of the content tag for translation  -->
+         
+
+
+
+
+
+
+
+
+
+
+        
+ 
+
+
+
+
+
+</div>
+<!-- content for translation-->
+
+
+
+
+
+
+
+<?php
+include('footer.php'); 
+?>
+</body>
+</html>
+<script src="search.js"></script>
